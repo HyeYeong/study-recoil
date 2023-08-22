0342bc8432d71409a3dd4ea6148c21e6222b64cd
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-08-22 18:16:09 +0900

    chor: setting recoil and file directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>study recoil</h1>
-      <p>with next js</p>
-    </main>
-  );
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>study-recoil</title>
+      </Head>
+      <main>
+        <h1>study recoil</h1>
+        <p>with next js</p>
+      </main>
+    </div>
+  );
+}
