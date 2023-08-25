831d18131c8d6fd78a00af820daa87c25119aa3a
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-08-25 13:32:49 +0900

    fix: counter component

--- src/components/organisms/CharacterCounter/index.tsx
+++ src/components/organisms/CharacterCounter/index.tsx
@@ -1,13 +1,10 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { ChangeEvent } from 'react';
+import { useRecoilState } from 'recoil';
 import { textState } from '@src/recoil/counter/atoms';
-import { charCountState } from '@src/recoil/counter/selectors';
 import { TextInput } from '@src/components/atoms/TextInput';
 import { CharacterCount } from '@src/components/atoms/CharacterCount';
 
 export const CharacterCounter = () => {
   const [text] = useRecoilState(textState);
-  const count = useRecoilValue(charCountState);
 
   return (
     <article>
